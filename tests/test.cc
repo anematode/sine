d46f348f8a986f28fe942bb8e3ec77475ea1cd43
anematode <timothy.herchen@gmail.com>
2018-02-12 19:54:49 -0800

    gah

--- tests/test.cc
+++ tests/test.cc
@@ -17,9 +17,9 @@ int main() {
 
             Sine::General::DefaultTimer timer{"bresenham-perf"};
 
-            for (int i = 0; i < 10; i++) {
-                Algorithms::drawMaskedBresenhamThick(width / 2, height / 2, width / 2 + 1000 * std::sin(i),
-                                                     height / 2 + 1000 * std::cos(i), 5, 0, 0, width, height,
+            for (int i = 0; i < 1000; i++) {
+                Algorithms::drawMaskedBresenham(width / 2 + 1000 * std::sin(i), height / 2 + 1000 * std::cos(i),
+                                                width / 2, height / 2, 1900, 1900, width, height,
                                                 [&](int x, int y) {
                                                     image_temp.setPixelNoThrow(x, y, RGBA(255, 255, 255, 255));
                                                 });
